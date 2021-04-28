b4246ae44eec7c0a6124506584bd392644c9b38a
Tu Nguyen <tunhbd1998@gmail.com>
2021-04-28 16:19:43 +0700

    init

---
+++ dist/rabbitmq-client.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.RabbitMQClient = void 0;
+var RabbitMQClient = /** @class */ (function () {
+    function RabbitMQClient() {
+    }
+    return RabbitMQClient;
+}());
+exports.RabbitMQClient = RabbitMQClient;
+//# sourceMappingURL=rabbitmq-client.js.map
